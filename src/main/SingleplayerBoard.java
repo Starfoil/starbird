fa0aefd224ac1a682364d783a82bedb9c2e57f66
eunsdaily <mount_e@naver.com>
2017-06-06 03:18:23 +0900

    1. refactoring 2. main/SingleplayerBoard 3. It does several things in one method. So I extract method.

--- src/main/SingleplayerBoard.java
+++ src/main/SingleplayerBoard.java
@@ -77,11 +77,87 @@ public class SingleplayerBoard extends JPanel implements ActionListener{
 	}
 
 	public void paint(Graphics g) {
-		//Background
-		g.clearRect(0, 0, MainMenu.XFRAME, MainMenu.YFRAME);
-		g.drawImage(background, background.getWidth(null) - backgroundPos1, 0, null);
-		g.drawImage(background, background.getWidth(null) - backgroundPos2, 0, null);
+		backgroundPaint(g);
+		statebarPaint(g);
+		textLabelPaint(g);
 
+		playerPaint(g);
+		enemyPaint(g);
+		botPaint(g);
+		coinPaint(g);
+		notExistPlayerStatePaint(g);
+	}
+
+	private void notExistPlayerStatePaint(Graphics g) {
+		// All players dead
+		if (game.gameStatus == 3){
+			g.setColor(Color.BLACK);
+			g.drawImage(SystemData.defeatIcon, 500, 150, null);
+			trophyCoinDraw(g); 
+			g.drawString("Defeat", 560, 345);
+			scordCoinDraw(g);
+		}
+		if (game.gameStatus == 4){
+			g.setColor(Color.BLACK);
+			g.drawImage(SystemData.victoryIcon, 500, 150, null);
+			trophyCoinDraw(g); 
+			g.drawString("Victory", 555, 345);
+			scordCoinDraw(g);
+		}
+	}
+
+	private void trophyCoinDraw(Graphics g) {
+		g.drawImage(SystemData.trophyIcon, 550, 375, null);
+		g.drawImage(SystemData.coinImage, 550, 445, null);
+		g.setFont(FontData.getInstance().getFont("Calibri", Font.BOLD, 28));
+	}
+
+	private void scordCoinDraw(Graphics g) {
+		g.setFont(FontData.getInstance().getFont("Arial", Font.PLAIN, 24)); 
+		g.drawString(Integer.toString(game.score), 610, 410);
+		g.drawString(Integer.toString(game.coinsCollected), 610, 480);
+	}
+
+	private void coinPaint(Graphics g) {
+		// Draw coins
+		for (CoinDrop d : game.drops){
+			d.draw(g, SystemData.coinImage);
+		}
+	}
+
+	private void botPaint(Graphics g) {
+		// Draw bots
+		for (Birdbot b : game.bots){
+			b.draw(g, SystemData.playerImages, SystemData.bulletImages);
+		}
+	}
+
+	private void enemyPaint(Graphics g) {
+		// Draw enemies
+		for (EnemyEntity e : game.birds){
+			e.draw(g);
+		}
+	}
+
+	private void playerPaint(Graphics g) {
+		// Draw player
+		for (Player p : game.players){
+			p.draw(g, SystemData.playerImages, SystemData.bulletImages);
+		}
+	}
+
+	private void textLabelPaint(Graphics g) {
+		// Draw text labels
+		g.setColor(Color.black);
+		g.setFont(new Font("Serif", Font.PLAIN, 24)); 
+		g.drawImage(SystemData.trophyIcon, 10, 40, null);
+		g.drawString(Integer.toString(game.score), 65, 75);
+		g.drawImage(SystemData.coinImage, 10, 100, null);
+		g.drawString(Integer.toString(game.coinsCollected), 65, 135);
+		g.drawString("Distance : " + game.distance, 10, 620);
+	}
+
+	private void statebarPaint(Graphics g) {
 		// Draw health/mana bars
 		g.setColor(Color.red);
 		g.fillRect(750, 20, (int) p.maxMana, 10);
@@ -90,59 +166,13 @@ public class SingleplayerBoard extends JPanel implements ActionListener{
 		g.fillRect(750, 20, (int) p.mana, 10);
 		g.setColor(Color.green);
 		g.fillRect(50, 20, (int) p.health, 10);
-		
-		// Draw text labels
-		g.setColor(Color.black);
-		g.setFont(FontData.getInstance().getFont("Serif", Font.PLAIN, 24)); 
-		g.drawImage(SystemData.trophyIcon, 10, 40, null);
-		g.drawString(Integer.toString(game.score), 65, 75);
-		g.drawImage(SystemData.coinImage, 10, 100, null);
-		g.drawString(Integer.toString(game.coinsCollected), 65, 135);
-		g.drawString("Distance : " + game.distance, 10, 620);
+	}
 
-		// Draw player
-		for (Player p : game.players){
-			p.draw(g, SystemData.playerImages, SystemData.bulletImages);
-		}
-
-		// Draw enemies
-		for (EnemyEntity e : game.birds){
-			e.draw(g);
-		}
-
-		// Draw bots
-		for (Birdbot b : game.bots){
-			b.draw(g, SystemData.playerImages, SystemData.bulletImages);
-		}
-
-		// Draw coins
-		for (CoinDrop d : game.drops){
-			d.draw(g, SystemData.coinImage);
-		}
-		
-		// All players dead
-		if (game.gameStatus == 3){
-			g.setColor(Color.BLACK);
-			g.drawImage(SystemData.defeatIcon, 500, 150, null);
-			g.drawImage(SystemData.trophyIcon, 550, 375, null);
-			g.drawImage(SystemData.coinImage, 550, 445, null);
-			g.setFont(FontData.getInstance().getFont("Calibri", Font.BOLD, 28)); 
-			g.drawString("Defeat", 560, 345);
-			g.setFont(FontData.getInstance().getFont("Arial", Font.PLAIN, 24)); 
-			g.drawString(Integer.toString(game.score), 610, 410);
-			g.drawString(Integer.toString(game.coinsCollected), 610, 480);
-		}
-		if (game.gameStatus == 4){
-			g.setColor(Color.BLACK);
-			g.drawImage(SystemData.victoryIcon, 500, 150, null);
-			g.drawImage(SystemData.trophyIcon, 550, 375, null);
-			g.drawImage(SystemData.coinImage, 550, 445, null);
-			g.setFont(FontData.getInstance().getFont("Calibri", Font.BOLD, 28)); 
-			g.drawString("Victory", 555, 345);
-			g.setFont(FontData.getInstance().getFont("Arial", Font.PLAIN, 24)); 
-			g.drawString(Integer.toString(game.score), 610, 410);
-			g.drawString(Integer.toString(game.coinsCollected), 610, 480);
-		}
+	private void backgroundPaint(Graphics g) {
+		//Background
+		g.clearRect(0, 0, MainMenu.XFRAME, MainMenu.YFRAME);
+		g.drawImage(background, background.getWidth(null) - backgroundPos1, 0, null);
+		g.drawImage(background, background.getWidth(null) - backgroundPos2, 0, null);
 	}
 
 	private class AL extends KeyAdapter {
