99b8e7b2f0fed99c8feeda4e7ac213fe762c2d4a
suheeeee <lalune1120@hotmail.com>
2017-06-07 23:08:53 +0900

    1. no pattern just refactoring(extra method and pull up) 2. The refactoring targets are StateMehods, StateAllPlayersDead, StateAllEnemiesDead, StateOngoingGame. 3. duplicated method.

--- src/main/StateAllEnemiesDead.java
+++ src/main/StateAllEnemiesDead.java
@@ -1,11 +1,5 @@
 package main;
 
-import java.util.ArrayList;
-
-import birds.EnemyEntity;
-import birds.EnemyUnit;
-import birds.Spawner;
-
 public class StateAllEnemiesDead extends StateMethods implements State {
 	private CollisionCheck collisionCheck = new CollisionCheck();
 
@@ -13,56 +7,27 @@ public class StateAllEnemiesDead extends StateMethods implements State {
 	public void perform(GameManager gameManager, GameInstance gameInstance) {
 		// TODO Auto-generated method stub
 
-		// spawnEnemies
-		for (Spawner s : gameInstance.birdSpawn) {
-			if (s.alive)
-				s.spawn();
-		}
-		// updateEnemies
-		for (EnemyEntity b : gameInstance.birds) {
-			b.update();
-		}
-		// updateBots
-		for (Birdbot b : gameInstance.bots) {
-			b.update();
-		}
-		// updateTargets
-		for (Birdbot b : gameInstance.bots) {
-			b.target = (EnemyUnit) botGetTarget(b, gameInstance);
-		}
-		for (EnemyEntity e : gameInstance.birds) {
-			e.playerTarget = enemyGetTarget(e, gameInstance);
-		}
-		// updateObjects
-		for (CoinDrop d : gameInstance.drops) {
-			d.update();
-		}
+		spawnEnemies(gameInstance);
+		updateEnemies(gameInstance);
+		updateBots(gameInstance);
+		updateTargets(gameInstance);
+		updateObjects(gameInstance);
+		
 		// cleanDeadObjects
-		// Remove dead mobs
-		ArrayList<EnemyEntity> enemyRemoveList = new ArrayList<EnemyEntity>();
-		for (EnemyEntity e : gameInstance.birds) {
-			if (e.xpos < -200 || e.ypos > 700 || e.isDead()) {
-				enemyRemoveList.add(e);
-			}
-		}
-		gameInstance.birds.removeAll(enemyRemoveList);
-
-		// Remove dead bots
-		ArrayList<Birdbot> botRemoveList = new ArrayList<Birdbot>();
-		for (Birdbot b : gameInstance.bots) {
-			if (b.health == 0) {
-				botRemoveList.add(b);
-			}
-		}
-		gameInstance.bots.removeAll(botRemoveList);
+		removeDeadMobs(gameInstance);
+		removeDeadBots(gameInstance);
 
 		collisionCheck.checkDropCoinCollision(gameInstance);
+		setStatus(gameInstance);
+		gameInstance.distance++;
+	}
+
+	private void setStatus(GameInstance gameInstance) {
 		gameInstance.gameEndTimer += 1;
 
 		if ((gameInstance.gameStatus == 3 || gameInstance.gameStatus == 4) && gameInstance.gameEndTimer > 500) {
 			gameInstance.gameStatus = -1;
 		}
-		gameInstance.distance++;
 	}
 
 }
--- src/main/StateOngoingGame.java
+++ src/main/StateOngoingGame.java
@@ -1,9 +1,5 @@
 package main;
 
-import java.util.ArrayList;
-
-import birds.EnemyEntity;
-import birds.EnemyUnit;
 import birds.Spawner;
 
 public class StateOngoingGame extends StateMethods implements State  {
@@ -13,48 +9,14 @@ public class StateOngoingGame extends StateMethods implements State  {
 	public void perform(GameManager gameManager, GameInstance gameInstance) {
 		// TODO Auto-generated method stub
 		boolean aliveflag = false;
-		// spawnEnemies
-		for (Spawner s : gameInstance.birdSpawn) {
-			if (s.alive)
-				s.spawn();
-		}
-		// updateEnemies
-		for (EnemyEntity b : gameInstance.birds) {
-			b.update();
-		}
-		// updateBots
-		for (Birdbot b : gameInstance.bots) {
-			b.update();
-		}
-		// updateTargets
-		for (Birdbot b : gameInstance.bots) {
-			b.target = (EnemyUnit) botGetTarget(b, gameInstance);
-		}
-		for (EnemyEntity e : gameInstance.birds) {
-			e.playerTarget = enemyGetTarget(e, gameInstance);
-		}
-		// updateObjects
-		for (CoinDrop d : gameInstance.drops) {
-			d.update();
-		}
-		// cleanDeadObjects
-		// Remove dead mobs
-		ArrayList<EnemyEntity> enemyRemoveList = new ArrayList<EnemyEntity>();
-		for (EnemyEntity e : gameInstance.birds) {
-			if (e.xpos < -200 || e.ypos > 700 || e.isDead()) {
-				enemyRemoveList.add(e);
-			}
-		}
-		gameInstance.birds.removeAll(enemyRemoveList);
-
-		// Remove dead bots
-		ArrayList<Birdbot> botRemoveList = new ArrayList<Birdbot>();
-		for (Birdbot b : gameInstance.bots) {
-			if (b.health == 0) {
-				botRemoveList.add(b);
-			}
-		}
-		gameInstance.bots.removeAll(botRemoveList);
+		spawnEnemies(gameInstance);
+		updateEnemies(gameInstance);
+		updateBots(gameInstance);
+		updateTargets(gameInstance);
+		updateObjects(gameInstance);
+		removeDeadMobs(gameInstance);
+		removeDeadBots(gameInstance);
+		
 		collisionCheck.checkCollision(gameInstance);
 		for (Player p : gameInstance.players) {
 			if (p.health > 0)
@@ -65,6 +27,12 @@ public class StateOngoingGame extends StateMethods implements State  {
 			if (s.alive)
 				enemyflag = true;
 		}
+		
+		setStatus(gameManager, gameInstance, aliveflag, enemyflag);
+		gameInstance.distance++;
+	}
+
+	private void setStatus(GameManager gameManager, GameInstance gameInstance, boolean aliveflag, boolean enemyflag) {
 		if (!aliveflag && gameInstance.gameStatus == 2) {
 			gameInstance.gameStatus = 3;
 			gameManager.setState(new StateAllPlayersDead());
@@ -72,7 +40,6 @@ public class StateOngoingGame extends StateMethods implements State  {
 			gameInstance.gameStatus = 4;
 			gameManager.setState(new StateAllEnemiesDead());
 		}
-		gameInstance.distance++;
 	}
 
 
